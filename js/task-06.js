73586e843784f99a0079a9bb6cb27fc34c879722
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2022-12-11 13:19:05 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#validation-input');
 
 input.addEventListener('blur', () => {
     const length = input.value.length;
-    const num = Number(input.getAttribute('data-length'))
+    const num = Number(input.getAttribute('data-length'));
 
     if (length === num) {
        return input.classList.add('valid');
